42f1651db275cd6af4744bf0c0c56dd31d1c6da6
Dom Wakeling <domwakeling@gmail.com>
2017-06-11 21:51:10 +0100

    Enable search functionality
    Search bar contents sync with IndexPage state
    
    Searching retrieves an authorisation token (or generates a new one) and
    then retrieves local 'bars'. At present this is then being logged to
    the console.

--- imports/ui/IndexPage.jsx
+++ imports/ui/IndexPage.jsx
@@ -4,6 +4,7 @@ import Layout from './components/Layout.jsx';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 import Search from './components/Search.jsx';
+import { Session } from 'meteor/session';
 // import { Votes } from '../api/votes.js';
 // import { Bert } from 'meteor/themeteorchef:bert';
 // import { _ } from 'underscore';
@@ -19,16 +20,70 @@ class IndexPage extends React.Component {
         };
         this.handleLocationChange = this.handleLocationChange.bind(this);
         this.handleLocationSubmit = this.handleLocationSubmit.bind(this);
+        this.promiseAuthToken = this.promiseAuthToken.bind(this);
     }
 
+    // Passed as callback to 'Search' component, keeping state aligned
     handleLocationChange(str) {
         this.setState({ searchLocation: str});
     }
 
-    // TODO: make this do something interesting
-    handleLocationSubmit(str) {
-        console.log('Received location:', str);
+    // Passed as callback to 'Search' component, activating a search
+    async handleLocationSubmit(str) {
+
+        str = str.replace(/\s/g, '%20');
+        this.promiseAuthToken().then( function(response) {
+            Meteor.call('yelp.getLocalInfo', response, str, function(err, res) {
+                if (err) {
+                    console.log('Error while asking for info', err);
+                } else {
+                    console.log('Result:', res);
+                }
+            });
+        }, function(error) {
+            console.log('Error', error);
+        });
     }
+
+    // Promise to return an auth token
+    async promiseAuthToken() {
+
+        return new Promise(function(resolve, reject) {
+
+            // set up variables
+            var authToken = '';
+            var lastUpdated = 0;
+            var tokenExpiry = 0;
+            var rightNow = Date.now();
+
+            // if we have a token stored in Session, get the details
+            if (Session.get('authToken')) {
+                const sessionToken = Session.get('authToken');
+                authToken = sessionToken.token;
+                lastUpdated = sessionToken.date;
+                tokenExpiry = sessionToken.expiry;
+            }
+
+            // if we have a current token, resolve with it
+            if (lastUpdated + 1000 * tokenExpiry > rightNow) {
+                resolve(authToken);
+            } else {
+                // don't have a current token, try to get a new one and store it
+                getNewAuthToken().then(function(newToken) {
+                    const newSessionToken = {
+                        token: newToken.access_token,
+                        expiry: newToken.expires_in,
+                        date: rightNow
+                    };
+                    Session.set('authToken', newSessionToken);
+                    resolve(newToken.access_token);
+                }, function(error) {
+                    reject(error);
+                });
+            }
+        });
+    }
+
 
     renderSearch() {
         const searchLocation = this.state.searchLocation;
@@ -50,6 +105,21 @@ class IndexPage extends React.Component {
             </Layout>
         );
     }
+}
+
+// Utility function to get new token using promise; outside of object
+async function getNewAuthToken() {
+
+    return new Promise(function(resolve, reject) {
+
+        Meteor.call('yelp.getAuthToken', function(err, res) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        });
+    });
 }
 
 // Define props types, error checking and prevents eslint error reports
--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import '/imports/api/votes.js';
+import '/imports/api/yelp.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
